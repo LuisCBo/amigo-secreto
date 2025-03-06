064b40d43ed1c4c273bb0d9a5925f183bb635503
Luis Bonilla <bonilla.lc12@gmail.com>
2025-03-05 21:07:37 -0500

    "UPDATE app.js"

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo");
+    nombre = nombre.value.trim();
+
+    if (nombre === "") {
+        alert("ERROR - Debe ingresar un nombre")
+    }
+}
