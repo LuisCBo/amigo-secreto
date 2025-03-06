b9f2f6298398a42819aacf6d6538791121f969b9
Luis Bonilla <bonilla.lc12@gmail.com>
2025-03-05 21:52:18 -0500

    "UPDATE 2 app.js"

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ function agregarAmigo() {
     nombre = nombre.value.trim();
 
     if (nombre === "") {
-        alert("ERROR - Debe ingresar un nombre")
+        alert("Por favor, inserte un nombre")
+    } else {
+        amigos.push(nombre);
+        actualizarLista();
+        nombre.value = "";
+    }
+
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
     }
 }
